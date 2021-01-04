792bcc46df7d19311f94aed51b9b9dc09d68b7c1
FESLARD Margaux <margaux@les-tilleuls.coop>
2021-01-04 13:29:36 +0100

    index with list of teddies from API

--- public/js/script.js
+++
@@ -1,9 +0,0 @@
-let request = new XMLHttpRequest();
-request.onreadystatechange = function() {
-    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-        let response = JSON.parse(this.responseText);
-        console.log(response);
-    }
-};
-request.open("GET", "http://localhost:3000/api/teddies");
-request.send();
