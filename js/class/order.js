c9c183e00677b6f82e97fd34e5ab9d34865f915b
FESLARD Margaux <margaux@les-tilleuls.coop>
2021-01-05 17:17:04 +0100

    panier

--- js/class/order.js
+++ js/class/order.js
@@ -1,4 +1,16 @@
+class OrderContact {
+    constructor(firstName, lastName, address, city, email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.email = email;
+    }
+}
+
 class Order {
-    constructor() {
+    constructor(products, orderId) {
+        this.products = products;
+        this.orderId = orderId;
     }
 }
--- js/class/product.js
+++ js/class/product.js
@@ -1,7 +1,9 @@
 class Product {
-    constructor(_id, name, price) {
+    constructor(_id, name, description, price, imageUrl) {
         this._id = _id;
         this.name = name;
+        this.description = description;
         this.price = price;
+        this.imageUrl = imageUrl;
     }
 }
